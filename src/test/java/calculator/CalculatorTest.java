49057ca4cbde95a9fb5f7e20b80cd49790f774d7
Pavel <pavbia@st.edu.amu.pl>
2024-02-10 22:53:21 +0100

    Added additional test

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -15,8 +15,92 @@ public class CalculatorTest {
     }
 
     @Test
+    public void powerTwoNumbersTest() {
+        Assert.assertEquals(64.0, calculator.powerTwoNumbers(4.0,3.0),0.0);
+    }
+
+    @Test
+    public void sqrtTest() {
+        Assert.assertEquals(8.0, calculator.sqrt(64.0),0.0);
+    }
+
+    @Test
+    public void CalculateSquareAreaTest() {
+        Assert.assertEquals(4.0, calculator.CalculateSquareArea(2),0.0);
+    }
+
+    @Test
+    public void CalculateRectAreaTest() {
+        Assert.assertEquals(8.0, calculator.CalculateRectArea(2,4),0.0);
+    }
+
+    @Test
+    public void CalculateSquareCircTest() {
+        Assert.assertEquals(4, calculator.CalculateSquareCirc(1));
+    }
+
+    @Test
     public void CalculateRectCircTest() {
         Assert.assertEquals(8, calculator.CalculateRectCirc(2,2));
+    }
+
+    @Test
+    public void CalculateTriangleAreaTest() {
+        Assert.assertEquals(6.0, calculator.CalculateTriangleArea(4,3),0.0);
+    }
+
+    @Test
+    public void CalculateTriangleCircTest() {
+        Assert.assertEquals(12.0, calculator.CalculateTriangleCirc(3,4,5),0.0);
+    }
+
+    @Test
+    public void convertCelsiusToFarenheitTest() {
+        Assert.assertEquals(68.0, calculator.convertCelsiusToFarenheit(20),0.0);
+    }
+
+    @Test
+    public void convertFarenheitToCelsiusTest() {
+        Assert.assertEquals(16.0, calculator.convertFarenheitToCelsius(60),0.0);
+    }
+
+    @Test
+    public void convertFromMphToKphTest() {
+        Assert.assertEquals(161.0, calculator.convertFromMphToKph(100),0.0);
+    }
+
+    @Test
+    public void factorialTest() {
+        Assert.assertEquals(24, calculator.factorial(4));
+    }
+
+    @Test
+    public void fibbonacciTest() {
+        Assert.assertEquals(21, calculator.fibbonacci(8));
+    }
+
+    @Test
+    public void findGCDTest() {
+        Assert.assertEquals(1, calculator.findGCD(8,3));
+    }
+
+    @Test
+    public void findLCMTest() {
+        Assert.assertEquals(24, calculator.findLCM(8,12));
+    }
+
+    @Test
+    public void isPrimeTrueTest() {
+        boolean prime = calculator.isPrime(7);
+        Assert.assertTrue(prime);
+    }
+
+    @Test
+    public void isPrimeFalseTest() {
+        boolean prime = calculator.isPrime(1);
+        Assert.assertFalse(prime);
+        prime = calculator.isPrime(8);
+        Assert.assertFalse(prime);
     }
 
 }
